fc57d7d571a0c69690c3c79d3a9e41c0c7d59a0a
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-03-12 18:22:46 -0400

    multi form dream done

--- src/components/addAttractions.component.js
+++ src/components/addAttractions.component.js
@@ -14,13 +14,24 @@ export default class addAttractions extends Component {
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-        attraction: '',
-        address: '',
-        image: '',
-        description: '',
-        ratings: '',
-        user: ''
+      currentStep: 1, // added state to keep track of current step
+      attraction: '',
+      address: '',
+      image: '',
+      description: '',
+      ratings: '',
+      user: ''
     }
+  }
+
+  nextStep = () => {
+    const { currentStep } = this.state;
+    this.setState({ currentStep: currentStep + 1 });
+  }
+
+  prevStep = () => {
+    const { currentStep } = this.state;
+    this.setState({ currentStep: currentStep - 1 });
   }
 
   onChangeAttraction(e) {
@@ -61,115 +72,133 @@ export default class addAttractions extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-
-    const attraction = {
-        attraction: this.state.attraction,
-        address: this.state.address,
-        image: this.state.image,
-        description: this.state.description,
-        ratings: this.state.ratings,
-        user: this.state.user
-    }
-    axios.post('http://localhost:5000/attractions/add', attraction)
-    .then(res => console.log(res.data));
-    console.log(attraction);
-
-    window.location = '/attractions';
-    
-    
+    this.nextStep(); // move to next step
   }
   
   render() {
+    const { currentStep } = this.state;
     return (
       <div className="container-add" style={{ margin: '100px' }}>
-  <form onSubmit={this.onSubmit}>
-    <div className="row setup-content" id="step-1">
-      <div className="col-md-6 offset-md-3">
-        <h1 className="text-center">Attraction</h1>
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.attraction}
-            onChange={this.onChangeAttraction}
-            placeholder="Enter Attraction"
-          />
-        </div>
-        <br />
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.address}
-            onChange={this.onChangeAddress}
-            placeholder="Enter Address"
-          />
-        </div>
-      </div>
+        <form onSubmit={this.onSubmit}>
+          {currentStep === 1 && (
+            <div className="row setup-content" id="step-1">
+              <div className="col-md-6 offset-md-3">
+                <h1 className="text-center">Attraction</h1>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.attraction}
+                    onChange={this.onChangeAttraction}
+                    placeholder="Enter Attraction"
+                  />
+                </div>
+                <br />
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.address}
+                    onChange={this.onChangeAddress}
+                    placeholder="Enter Address"
+                  />
+                </div>
+                <br />
+                <div className="form-group text-center">
+  <div className="row justify-content-between">
+    <div className="col-6">
+      <button className="btn btn-primary btn-lg" type="submit">Preview</button>
     </div>
-    <div className="row setup-content" id="step-2">
-      <div className="col-md-6 offset-md-3">
-        <h1 className="text-center">Detail</h1>
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.image}
-            onChange={this.onChangeImage}
-            placeholder="Enter Image URL"
-          />
-        </div>
-        <br />
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.description}
-            onChange={this.onChangeDescription}
-            placeholder="Enter Description"
-          />
-        </div>
-      </div>
+    <div className="col-6 text-right">
+      <button className="btn btn-success btn-lg" type="submit">Next</button>
     </div>
-    <div className="row setup-content" id="step-3">
-      <div className="col-md-6 offset-md-3">
-        <h1 className="text-center">Creator</h1>
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.ratings}
-            onChange={this.onChangeRatings}
-            placeholder="Enter Rating"
-          />
-        </div>
-        <br />
-        <div className="form-group">
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={this.state.user}
-            onChange={this.onChangeUser}
-            placeholder="Enter Your Name"
-          />
-        </div>
-        <div className="form-group text-center">
-        <br />
-          <button className="btn btn-primary btn-lg" type="submit">Submit</button>
-        </div>
-      </div>
-    </div>
-  </form>
+  </div>
 </div>
-
-
-
-);
-}
+              </div>
+            </div>
+          )}
+          {currentStep === 2 && (
+            <div className="row setup-content" id="step-2">
+              <div className="col-md-6 offset-md-3">
+                <h1 className="text-center">Detail</h1>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.image}
+                    onChange={this.onChangeImage}
+                    placeholder="Enter Image URL"
+                  />
+                </div>
+                <br />
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.description}
+                    onChange={this.onChangeDescription}
+                    placeholder="Enter Description"
+                  />
+                </div>
+                <br />
+                <div className="form-group text-center">
+  <div className="row justify-content-between">
+    <div className="col-6">
+      <button className="btn btn-primary btn-lg" type="submit">Preview</button>
+    </div>
+    <div className="col-6 text-right">
+      <button className="btn btn-success btn-lg" type="submit">Next</button>
+    </div>
+  </div>
+</div>
+              </div>
+            </div>
+          )}
+          {currentStep === 3 && (
+            <div className="row setup-content" id="step-3">
+              <div className="col-md-6 offset-md-3">
+                <h1 className="text-center">Creator</h1>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.ratings}
+                    onChange={this.onChangeRatings}
+                    placeholder="Enter Rating"
+                  />
+                </div>
+                <br />
+                <div className="form-group">
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={this.state.user}
+                    onChange={this.onChangeUser}
+                    placeholder="Enter Your Name"
+                  />
+                </div>
+                <br />
+                <div className="form-group text-center">
+  <div className="row justify-content-between">
+    <div className="col-6">
+      <button className="btn btn-primary btn-lg" type="submit">Preview</button>
+    </div>
+    <div className="col-6 text-right">
+      <button className="btn btn-success btn-lg" type="submit">Submit</button>
+    </div>
+  </div>
+</div>
+              </div>
+            </div>
+          )}
+        </form>
+      </div>
+    );
+  }
 }
