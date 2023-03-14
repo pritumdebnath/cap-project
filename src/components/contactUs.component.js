b62ede76031abde381dde20a930df08194e90b6f
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-03-14 19:48:56 -0400

    new push

--- src/components/contactUs.component.js
+++ src/components/contactUs.component.js
@@ -78,73 +78,71 @@ export default class contactUs extends Component {
   }
   render() {
     return (
-      <div class="container mt-5 shadow ">
-        <div class="row ">
-            <div class="col-md-4 bg-primary p-5 text-white order-sm-first order-last">
-                <h2>Let's get in touch</h2>
-                <p>We're open for any suggestion or just to have a chat</p>
+      <div class="containercontact mt-5 shadow ">
+        <div class="row">
+            <div class="col-md-4 bg-primarycontact p-5 text-white order-sm-first order-last">
+                <h2>Queen City Attractions</h2>
+                {/* <p>We're open for any comment or suggestion!</p> */}
+                <div className='sidebarcontact'>
                 <div class="d-flex mt-2">
                     <i class="bi bi-geo-alt"></i>
-                    <p class="mt-3 ms-3">Address : Road 198 West 21th Street, Suite 721 Singapor WW 10016</p>
+                    <p class="mt-3 ms-3 pcontact">Address : 1 Queen City Way, Cincinnati, OH 45201</p>
                 </div>
                 <div class="d-flex mt-2">
                     <i class="bi bi-telephone-forward"></i>
-                    <p class="mt-4 ms-3">Phone : 8888888888</p>
+                    <p class="mt-4 ms-3 pcontact">Phone : (513)000-0000</p>
                 </div>
                 <div class="d-flex mt-2">
                     <i class="bi bi-envelope"></i>
-                    <p class="mt-4 ms-3">Email : contactform@gmail.com</p>
+                    <p class="mt-4 ms-3 pcontact">Email : queencitytravel@email.com</p>
                 </div>
-                <div class="d-flex mt-2">
-                    <i class="bi bi-youtube"></i>
-                    <p class="mt-4 ms-3">Channel : www.contactform.com/</p>
                 </div>
             </div>
             <div class="col-md-8 p-5 ">
-                <h2>Get in touch</h2>
+                <h2>Leave a Comment!</h2>
                 <form class="row g-3 contactForm mt-4" id='contact-form' onSubmit={this.onSubmit}>
-                    <div class="col-md-6 form-group">
-                      <label for="inputEmail4" className="contact-label form-label">Name: </label>
-                      <input
-                        type="text email"
-                        required
-                        id="inputEmail4"
-                        className="form-control"
-                        value={this.state.name}
-                        onChange={this.onChangeName}
-                        >
-                      </input>
+                    <div class="col-12 form-group">
+                      <label for="inputAddress" class="contact-label form-label">Name: </label>
+                      <input 
+                      type="text email" 
+                      class="form-control" 
+                      id="inputAddress" 
+                      required 
+                      value={this.state.name} 
+                      onChange={this.onChangeName}/>
                     </div>
-                    {/* <div class="col-md-6">
-                      <label for="inputPassword4" class="form-label">Last Name</label>
-                      <input type="password" class="form-control" id="inputPassword4" required/>
-                    </div> */}
-                    <div class="col-12">
-                      <label for="inputAddress" class="form-label">Message</label>
-                      <input type="text" class="form-control" id="inputAddress" required/>
+                    <div class="col-12 form-group">
+                      <label for="inputAddress" class="contact-label form-label">Email: </label>
+                      <input 
+                      type="text email" 
+                      class="form-control" 
+                      id="inputAddress" 
+                      required 
+                      value={this.state.email}
+                      onChange={this.onChangeEmail}/>
                     </div>
-                    <div class="col-12">
-                        <label for="inputAddress" class="form-label">Email</label>
-                        <input type="text" class="form-control" id="inputAddress" required/>
+                    <div class="col-12 form-group">
+                      <label for="inputAddress" class="contact-label form-label">Phone Number: </label>
+                      <input 
+                      type="text email" 
+                      class="form-control" 
+                      id="inputAddress" 
+                      required 
+                      value={this.state.number}
+                      onChange={this.onChangeNumber}/>
                     </div>
-                    <div class="col-12">
-                      <label for="inputAddress" class="form-label">Message</label>
-                      <input type="text" class="form-control" id="inputAddress" required/>
+                    <div class="col-12 form-group">
+                      <label for="inputAddress" class="contact-label form-label">Comment: </label>
+                      <input 
+                      type="text email" 
+                      class="form-control" 
+                      id="inputAddress" 
+                      required 
+                      value={this.state.comment}
+                      onChange={this.onChangeComment}/>
                     </div>
-                    <div class="col-12">
-                        <label for="inputAddress" class="form-label">Email</label>
-                        <input type="text" class="form-control" id="inputAddress" required/>
-                    </div>
-                    <div class="col-md-6">
-                      <label for="inputCity" class="form-label">Phone Number</label>
-                      <input type="text" class="form-control" id="inputCity"/>
-                    </div>
-                    {/* <div class="col-md-6">
-                        <label for="inputCity" class="form-label">Contact Number</label>
-                        <input type="text" class="form-control" id="inputCity" required/>
-                      </div> */}
-                    <div class="col-12">
-                      <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                    <div class="col-12 form-group">
+                      <button type="submit" class="btn1 btn-primary1 mt-3" value="Send">Submit</button>
                     </div>
                   </form>
             </div>
