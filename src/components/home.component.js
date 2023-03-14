71298cfb0c2047b7e23c3bc3a852ff6989bc5c6d
gigyen <coolllyod@gmail.com>
2023-03-13 23:39:39 -0400

    added pride of cincinnati

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -3,74 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import image1 from "../assets/cinci1.jpg";
 import image2 from "../assets/cinci2.jpg";
 import image3 from "../assets/cinci3.jpg";
+import image4 from "../assets/soccer.png";
+import image5 from "../assets/reds.png";
+import image6 from "../assets/bengals.png";
+
 import "../components/style/home.style.css";
 
 export default class Home extends Component {
   render() {
     return (
-      
-      <div id="home-section" style={{position: 'relative'}}>
-      <h2 className="text-center hhh">This is Cincinnati!</h2>
-      <hr />
-      <section className="destinations container">
-        <div className="row justify-content-center home-cards">
-          <div className="col-md-4 mt-5 mb-3 ">
-            <div className="card profile-card-5">
-              <div className="card-img-block">
-                <img
-                  className="card-img-top"
-                  src={image1}
-                  alt="brentspence" />
-              </div>
-              <div className="card-body pt-0">
-                <h5 className="hhh card-title text-center m-3">Brent Spence Bridge</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make
-                  up the bulk of the card's content.
-                </p>
-              </div>
+      <div id="home-section" style={{ position: "relative" }}>
+        <h2 className="text-center">Pride of Cincinnati!</h2>
+        <hr />
+        <section className="destinations container-fluid col-lg-11 col-md-10 col-sm-12 mt-5 mb-3">
+          <div className="row">
+            <div className="col-lg-4 col-sm-6 text-center">
+              <h5 className="p-3 hhh">FC Cincinnati</h5>
+              <img className="home-images" src={image4} alt="brentspence" />
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+              <h5 className="p-3 hhh">Reds</h5>
+              <img className="home-images" src={image5} alt="brentspence" />
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+              <h5 className="p-3 hhh">Bengals</h5>
+              <img className="home-images" src={image6} alt="brentspence" />
             </div>
           </div>
-
-          <div className="col-md-4 mt-5 mb-3">
-            <div className="card profile-card-5">
-              <div className="card-img-block">
-                <img
-                  className="card-img-top"
-                  src={image2}
-                  alt="paycor" />
-              </div>
-              <div className="card-body pt-0">
-                <h5 className="hhh card-title text-center m-3">Paycor Stadium</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make
-                  up the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 mt-5 mb-3">
-            <div className="card profile-card-5">
-              <div className="card-img-block">
-                <img
-                  className="card-img-top"
-                  src={image3}
-                  alt="skyline" />
-              </div>
-              <div className="card-body pt-0">
-                <h5 className="hhh card-title text-center m-3">Cincinnati Skyline</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make
-                  up the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
       </div>
-      
     );
   }
 }
