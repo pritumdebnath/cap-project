6c3087193dd62ef2e2de66af3c86c55cd3996b3d
Douglas <mercurymuur86@yahoo.com>
2023-03-17 16:11:16 -0400

    push

--- src/components/attractions.component.js
+++ src/components/attractions.component.js
@@ -2,31 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';  
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-// import kingsIsland from "../assets/kingsIsland2.jpeg";
-// import greatAmerican from "../assets/greatAmerican.jpeg";
-// import paycorStadium from "../assets/paycorStadium.jpeg";
-// import tqlStadium from "../assets/tqlStadium.jpeg";
-// import heritageBankArena from "../assets/heritageBankArena.jpeg";
-// import ucCampus from "../assets/ucCampus.jpeg";
-// import xavierUniversity from "../assets/xavierUniversity.jpeg";
-// import cincinnatiZoo from "../assets/cincinnatiZoo.jpeg";
-// import taftTheater from "../assets/taftTheater.jpeg";
-//import cincinnatiSkyline from "../images/cincinnatiSkyline.jpeg";
-//import cincinnatiSportsLogos from "../assets/cincinnatiSportsLogos.png";
-
-=======
 import cincinnatiSkyline from '../assets/cincinnatiSkyline.jpeg';
->>>>>>> a4a5aa2cdbea481cf688e1a9bf8517d1a1b8e732
 import '../components/style/attractions.css';
 
 
 const Attraction = props => {
   return (
-<<<<<<< HEAD
-    <>
-=======
->>>>>>> a4a5aa2cdbea481cf688e1a9bf8517d1a1b8e732
     <div className='attractionscards'>
         <div class="card">     
           <div class="card-body">
@@ -73,7 +54,6 @@ export default class Attractions extends Component {
     
   render() {
     return (
-<<<<<<< HEAD
   <div>
   <div className='skyline'>
   </div>
@@ -94,20 +74,6 @@ export default class Attractions extends Component {
       <div class="card">     
         <div class="card-body">  
           {this.attractionList()}
-=======
-      
-      <div>
-        <div>
-          <img src={cincinnatiSkyline} alt="" className='skyline' />
-        </div>
-        <div className='backattractions'>
-          <h2 id="header">Posted Attractions</h2>
-          <div>
-              <div className='attractionscards'>
-                {this.attractionList()}
-              </div>
-          </div>
->>>>>>> a4a5aa2cdbea481cf688e1a9bf8517d1a1b8e732
         </div>
       </div>
       
