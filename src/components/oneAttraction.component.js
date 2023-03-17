c27beed600e9c5704debf6c93476cf59994e87f6
gigyen <coolllyod@gmail.com>
2023-03-17 10:39:58 -0400

    few change

--- src/components/oneAttraction.component.js
+++ src/components/oneAttraction.component.js
@@ -36,13 +36,13 @@ export default class oneAttraction extends Component {
   render() {
     return (
       <div className="one-attraction row align-content-center justify-content-center">
-        <div className="lefty col-6 col-md-4 col-8 text-center text-white">
+        <div className="lefty col-6 col-md-4 col-8 text-center text-white p-3">
           <h2 className="place-name">{this.state.attraction}</h2>
           <div className="place-photo">
             <img src={this.state.image} alt="." className="img-fluid p-3" />
           </div>
         </div>
-        <div className="righty col-6 col-md-4 col-8 bg-light">
+        <div className="righty col-6 col-md-4 col-8 bg-light p-3">
           <h5>{this.state.address}</h5>
           <p>{this.state.description}</p>
           <p>{this.state.ratings}</p>
