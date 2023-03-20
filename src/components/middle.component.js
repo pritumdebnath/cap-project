009902fdc9595dced420379a226395a705365108
gigyen <coolllyod@gmail.com>
2023-03-20 16:23:32 -0400

    weather api

--- src/components/middle.component.js
+++ src/components/middle.component.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "../components/style/middle.style.css";
+import Weather from "./Weather";
+
+// const api = {
+//   key: '8ab0564c2a88e77ff1bbb0402c22c9dc',
+//   base: 'http://api.openweathermap.org/data/2.5'
+
+// }
+
+// const []
 
 const Middle = () => {
   return (
-    <div className="container-fluid middle text-center">
-      <div>
-        <button className="btn btn-danger btn-sm mb-5">
-          Alert!
-        </button>
-        <p className="text-white">
-          From now on, sweet dreams are made of chili and sight-seeing in
-          Cincinnati
-        </p>
-      </div>
+    <div className="container-fluid middle text-center p-3">
+      <h3 className="p-3 text-white">Cincinnati weather</h3>
+      <Weather /> 
     </div>
   );
 };
