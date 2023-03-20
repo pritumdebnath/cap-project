f6b0910bdbad7c9999ccc009b561f2c499cc08b2
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-03-20 16:21:32 -0400

    new push

---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+// import React, { Component } from 'react'
+
+// export default class Filter extends Component {
+//     handleClick = () => {
+//         if({props.attraction.ratings} === 5){
+//             console.log('funcFired')
+//         } else if(){
+
+//         }
+//     }
+//     render() {
+//         return (
+//         <div>
+//             <button>3 Stars or Less</button>
+//             <button>4 Stars</button>
+//             <button>5 Stars</button>
+//         </div>
+//         )
+//     }
+// }
