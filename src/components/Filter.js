67798715d82e79fe329ca12fa48146f340383195
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-03-20 21:16:50 -0400

    filter complete

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -18,3 +18,22 @@
 //         )
 //     }
 // }
+
+// class Filter extends Component {
+//   handleClick = (rating) => {
+//     // Call the onFilter function passed in as a prop with the selected rating
+//     this.props.onFilter(rating);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={() => this.handleClick(3)}>3 Stars or Less</button>
+//         <button onClick={() => this.handleClick(4)}>4 Stars</button>
+//         <button onClick={() => this.handleClick(5)}>5 Stars</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Filter;
