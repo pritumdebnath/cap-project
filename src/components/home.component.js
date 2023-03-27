982344e5291ece253d19e289f70e8ad3ce3ba020
gigyen <coolllyod@gmail.com>
2023-03-27 10:36:26 -0400

    fix

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -57,21 +57,31 @@ export default class Home extends Component {
                       }}
                     />
                     <div style={{ position: "relative", top: "200px" }}>
-                      <img
-                        src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1034da38251751.575a73ca2a072.gif"
-                        alt="Marching Band"
-                        style={{ width: "200px", height: "200px" }}
-                      />
+                      <marquee behavior="scroll" direction="left">
+                        <img
+                          src="https://i.gifer.com/28g6.gif"
+                          alt="Fireworks"
+                          style={{
+                            width: "100%",
+                            height: "auto",
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                          }}
+                        />
+                      </marquee>
                     </div>
                     <div
                       scrollamount="50"
                       style={{ position: "relative", top: "200px" }}
                     >
-                      <img
-                        src="https://spng.pngfind.com/pngs/s/680-6803351_sad-cat-meme-banana-hd-png-download.png"
-                        alt="jojo bannana cat"
-                        style={{ width: "200px", height: "200px" }}
-                      />
+                      <marquee behavior="scroll" direction="left">
+                        <img
+                          src="https://spng.pngfind.com/pngs/s/680-6803351_sad-cat-meme-banana-hd-png-download.png"
+                          alt="jojo bannana cat"
+                          style={{ width: "200px", height: "200px" }}
+                        />
+                      </marquee>
                     </div>
                   </div>
                 )}
@@ -81,60 +91,6 @@ export default class Home extends Component {
                   alt="Responsive"
                 />
               </div>
-              <div className="col-lg-4 col-sm-4 text-center">
-                <h5 className="p-3 hhh">Bengals</h5>
-                <img
-                  className="home-images img-fluid"
-                  src={image6}
-                  alt="Responsive"
-                />
-              </div>
-      <Helmet>
-          <title>{ TITLE }</title>
-      </Helmet>
-      <div id="home-section" style={{ position: "relative" }}>
-        <h2 className="text-center pride">Pride of Cincinnati!</h2>
-        <hr />
-        <section className="destinations container my-5">
-          <div className="row text-center">
-            <div className="col-lg-4 col-sm-4 text-center">
-              <h5 className="p-3 hhh">FC Cincinnati</h5>
-              <img className="home-images img-fluid" src={image4} alt="responsive" />
-            </div>
-            <div className="col-lg-4 col-sm-4 text-center">
-              <h5 className="p-3 hhh" onClick={this.handleButtonClick}>Reds</h5>
-              {this.state.showGif && (
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <img
-                    src="https://i.gifer.com/28g6.gif"
-                    alt="Fireworks"
-                    style={{ width: '100%', height: 'auto', position: 'absolute', top: 0, left: 0 }}
-                  />
-                  <div  style={{ position: 'relative', top: '200px' }}>
-                  <marquee behavior="scroll" direction="left">
-                    <img
-                      src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1034da38251751.575a73ca2a072.gif"
-                      alt="Marching Band"
-                      style={{ width: '200px', height: '200px' }}
-                    />
-                    </marquee>
-                  </div>
-                  <div scrollamount='50' style={{ position: 'relative', top: '200px' }}>
-                  <marquee behavior="scroll" direction="left">
-                    <img
-                      src="https://spng.pngfind.com/pngs/s/680-6803351_sad-cat-meme-banana-hd-png-download.png"
-                      alt="jojo bannana cat"
-                      style={{ width: '200px', height: '200px' }}
-                    />
-                    </marquee>
-                  </div>
-                </div>
-              )}
-              <img className="home-images img-fluid" src={image5} alt="Responsive" />
-            </div>
-            <div className="col-lg-4 col-sm-4 text-center">
-              <h5 className="p-3 hhh">Bengals</h5>
-              <img className="home-images img-fluid" src={image6} alt="Responsive" />
             </div>
           </section>
         </div>
